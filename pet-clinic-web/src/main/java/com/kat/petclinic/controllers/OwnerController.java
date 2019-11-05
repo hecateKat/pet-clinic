b14f228c51cecb235034ffb481298237617dc455
Kat <kat.ochmanska@gmail.com>
2019-11-05 22:40:17 +0100

    Added Owners List and updated controller. Closes #16

--- pet-clinic-web/src/main/java/com/kat/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/kat/petclinic/controllers/OwnerController.java
@@ -1,14 +1,25 @@
 package com.kat.petclinic.controllers;
 
+import com.kat.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners") // its prefixing owners in requestMapping at method
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOwners(){
+    public String listOwners(Model model){
+
+        model.addAttribute("owners", ownerService.findAll());
+
         return "owners/index";
     }
 

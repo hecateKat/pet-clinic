f845bc2defa4dc46f856e9c2cb18b74bdd24a261
Kat <kat.ochmanska@gmail.com>
2019-11-06 15:50:46 +0100

    Added PetType to bootstrap. Closes #32

--- pet-clinic-web/src/main/java/com/kat/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/kat/petclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.kat.petclinic.bootstrap;
 
 import com.kat.petclinic.entities.Owner;
+import com.kat.petclinic.entities.PetType;
 import com.kat.petclinic.entities.Vet;
 import com.kat.petclinic.services.OwnerService;
+import com.kat.petclinic.services.PetTypeService;
 import com.kat.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,14 +14,27 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType cat = new PetType();
+        cat.setName("Dog");
+
+        PetType savedCatPetType = petTypeService.save(cat);
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+
+        PetType savedDogPetType = petTypeService.save(dog);
 
         Owner owner1 = new Owner();
         owner1.setName("Graham");

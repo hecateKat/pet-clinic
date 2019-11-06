54cb8ecead9b5388c217e9364bd0ba20f5b1925e
Kat <kat.ochmanska@gmail.com>
2019-11-06 16:07:01 +0100

    Added Pets and Contact Info to bootstrap. Closes #33

--- pet-clinic-web/src/main/java/com/kat/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/kat/petclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package com.kat.petclinic.bootstrap;
 
 import com.kat.petclinic.entities.Owner;
+import com.kat.petclinic.entities.Pet;
 import com.kat.petclinic.entities.PetType;
 import com.kat.petclinic.entities.Vet;
 import com.kat.petclinic.services.OwnerService;
@@ -8,6 +9,8 @@ import com.kat.petclinic.services.PetTypeService;
 import com.kat.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -39,6 +42,15 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setName("Graham");
         owner1.setSurname("Masterton");
+        owner1.setAddress("123 Darkshore");
+        owner1.setCity("Miami");
+        owner1.setTelephone("999666444");
+        Pet grahamsPet = new Pet();
+        grahamsPet.setName("Dracul");
+        grahamsPet.setPetType(savedDogPetType);
+        grahamsPet.setOwner(owner1);
+        grahamsPet.setBirthDay(LocalDate.now().minusMonths(6));
+        owner1.getPets().add(grahamsPet);
 
         ownerService.save(owner1);
 
@@ -46,6 +58,15 @@ public class DataLoader implements CommandLineRunner {
         Owner owner2 = new Owner();
         owner2.setName("Anne");
         owner2.setSurname("Rice");
+        owner2.setAddress("321 Tirisfal");
+        owner2.setCity("Stormwind");
+        owner2.setTelephone("888555333");
+        Pet anniesPet = new Pet();
+        anniesPet.setName("Luna");
+        anniesPet.setPetType(savedCatPetType);
+        anniesPet.setOwner(owner2);
+        anniesPet.setBirthDay(LocalDate.now().minusMonths(9));
+        owner2.getPets().add(anniesPet);
 
         ownerService.save(owner2);
 

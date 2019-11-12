74c2920379dc8fc23e4354d9315f97a796f2e1d4
Kat <kat.ochmanska@gmail.com>
2019-11-12 14:05:37 +0100

    added spring data jpa service for petTypes. Closes #42

---
+++ pet-clinic-data/src/main/java/com/kat/petclinic/services/springdatajpa/PetTypeSpringDataJpaService.java
@@ -0,0 +1,47 @@
+package com.kat.petclinic.services.springdatajpa;
+
+import com.kat.petclinic.entities.PetType;
+import com.kat.petclinic.repositories.PetTypeRepository;
+import com.kat.petclinic.services.PetTypeService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class PetTypeSpringDataJpaService implements PetTypeService {
+
+    private final PetTypeRepository petTypeRepository;
+
+    public PetTypeSpringDataJpaService(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+
+        Set<PetType> petTypes = new HashSet<>();
+
+        petTypeRepository.findAll().forEach(petTypes::add);
+
+        return petTypes;
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return petTypeRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return petTypeRepository.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        petTypeRepository.delete(object);
+    }
+
+    @Override
+    public void deteleById(Long id) {
+        petTypeRepository.deleteById(id);
+    }
+}

e6c2b20cb26441d980c910417115efe242af6584
Kat <kat.ochmanska@gmail.com>
2019-11-12 13:26:44 +0100

    Updated vet to use JPA. Closes #37

--- pet-clinic-data/src/main/java/com/kat/petclinic/entities/Vet.java
+++ pet-clinic-data/src/main/java/com/kat/petclinic/entities/Vet.java
@@ -1,10 +1,16 @@
 package com.kat.petclinic.entities;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
+    @ManyToMany(fetch = FetchType.EAGER) //JPA will load all at once
+    @JoinTable(name = "vet_specialities", joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {

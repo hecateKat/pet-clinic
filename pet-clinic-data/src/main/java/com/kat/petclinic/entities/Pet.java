3c494ddeb17e9e1a869afd7e26045abdf0be43ae
Kat <kat.ochmanska@gmail.com>
2019-11-15 10:27:42 +0100

    fixing small errors

--- pet-clinic-data/src/main/java/com/kat/petclinic/entities/Pet.java
+++ pet-clinic-data/src/main/java/com/kat/petclinic/entities/Pet.java
@@ -26,7 +26,7 @@ public class Pet extends BaseEntity {
     @JoinColumn(name = "owner_id")
     private Owner owner;
 
-    @Column(name = "birth_date")
+    @Column(name = "birth_day")
     private LocalDate birthDay;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
